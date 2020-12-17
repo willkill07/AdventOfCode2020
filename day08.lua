c7ed9e94c8efdb474fcd5d9aa83a6d5cb53d2d6b
Will Killian <william.killian@millersville.edu>
2020-12-17 13:00:19 -0500

    Update day 8 -- streamline code

--- day08.lua
+++ day08.lua
@@ -17,26 +17,20 @@ function simulate(instructions, backtrace, pc, acc)
     acc = acc or 0
     pc = pc or 1
     local visits = {}
-
     local machine = {
         ["nop"] = function(arg) pc = pc + 1 end,
         ["acc"] = function(arg) acc = acc + arg    pc = pc + 1 end,
         ["jmp"] = function(arg) pc = pc + arg end
     }
-
-    while true do
+    while visits[pc] == nil and (pc ~= #instructions) do
         table.insert(backtrace, {pc, acc})
-        
-        if visits[pc] ~= nil or (pc == #instructions) then
-            return acc
-        end
         visits[pc] = true
-        
         local ins = instructions[pc][1]
         local arg = instructions[pc][2]
-        
         machine[ins](arg)
     end
+    table.insert(backtrace, {pc, acc})
+    return acc
 end
 
 function part1(instructions, bc)
@@ -46,19 +40,18 @@ end
 function part2(instructions, bc)
     for i, b in ipairs(bc) do
         if instructions[b[1]][1] ~= "acc" then
-            toggle(instructions[b[1]])
             local new_bc = {}
+            toggle(instructions[b[1]])
             local acc = simulate(instructions, new_bc, b[1], b[2])
+            toggle(instructions[b[1]])
             if new_bc[#new_bc][1] == #instructions then
                 return acc
             end
-            toggle(instructions[b[1]])
         end
     end
 end
 
 local bc = {}
-
 local answer1 = part1(instructions, bc)
 local answer2 = part2(instructions, bc)
 
