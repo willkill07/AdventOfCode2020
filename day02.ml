7b0495ba37c71b549eb2a1e9da7aacce72179ebf
Will Killian <william.killian@millersville.edu>
2020-12-17 12:31:33 -0500

    Add day 17 input; update day 2

--- day02.ml
+++ day02.ml
@@ -1,17 +1,17 @@
-let pred1 lo hi c pw =
-  let count = List.init (String.length pw) (String.get pw) |> List.find_all ((=) c) |> List.length in
-  lo <= count && count <= hi
-
-let pred2 lo hi c pw =
-  (pw.[lo - 1] = c) <> (pw.[hi - 1] = c)
-
-let file_lines_fold (name:string) (comb:'res -> string -> 'res) (init:'res) : 'res =
+let file_lines_fold name comb init =
   let ic = open_in name
   in let try_read () =
     try Some (input_line ic) with End_of_file -> None
   in let rec loop acc =
     match try_read () with Some s -> loop (comb acc s) | None -> close_in ic; acc
   in loop init
+
+let pred1 lo hi c pw =
+  let count = List.init (String.length pw) (String.get pw) |> List.find_all ((=) c) |> List.length in
+  lo <= count && count <= hi
+
+let pred2 lo hi c pw =
+  (pw.[lo - 1] = c) <> (pw.[hi - 1] = c)
 
 let day02 filename part =
   let pred = if part = 1 then pred1 else pred2 in
