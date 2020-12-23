ae9117b988c517b8f6cc6ed632574dccc0829e28
Will Killian <william.killian@millersville.edu>
2020-12-22 23:54:40 -0500

    Update indentation for day 22

--- day22.ts
+++ day22.ts
@@ -20,26 +20,26 @@ const part1 = (p1 : number[], p2 : number[]) => {
 }
 
 const combat = (p1: number[], p2: number[]) : [number, number[]] => {
-	const seen = new Set<string>();
-	while (true) {
+  const seen = new Set<string>();
+  while (true) {
     if (p1.length === 0) return [2, p2];
     if (p2.length === 0) return [1, p1];
     const key = [p1, p2].map(x => x.join(',')).join('|');
-		if (seen.has(key)) {
-			return [1, p1];
-		}
-		seen.add(key);
-		const [t1, t2] = [p1.shift(), p2.shift()]
-		let win : number = t1 > t2 ? 1 : 2;
-		if (p1.length >= t1 && p2.length >= t2) {
-			[win, ] = combat(p1.slice(0, t1), p2.slice(0, t2));
-		}
-		if (win === 1) {
+    if (seen.has(key)) {
+      return [1, p1];
+    }
+    seen.add(key);
+    const [t1, t2] = [p1.shift(), p2.shift()]
+    let win : number = t1 > t2 ? 1 : 2;
+    if (p1.length >= t1 && p2.length >= t2) {
+      [win, ] = combat(p1.slice(0, t1), p2.slice(0, t2));
+    }
+    if (win === 1) {
       [t1, t2].forEach(x => p1.push(x))
-		} else {
+    } else {
       [t2, t1].forEach(x => p2.push(x))
-		}
-	}
+    }
+  }
 }
 
 const part2 = (p1: number[], p2: number[]) : number[] => combat(p1, p2)[1]
